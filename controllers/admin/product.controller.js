31b6d51f4f2cd094b3df377f04010c8150c1ce28
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-16 19:51:37 +0700

    1. Sắp xếp các sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -33,9 +33,18 @@ module.exports.index = async (req, res) => {
 
   // End Pagination
 
+  // Sort
+  let sort = {};
+  if (req.query.sortKey && req.query.sortValue) {
+    sort[req.query.sortKey] = req.query.sortValue;
+  } else {
+    sort.position = "desc";
+  }
+  // END Sort
+
   // lọc ra những sản phẩm thỏa mãn
   const products = await Product.find(find)
-    .sort({ position: "desc" })
+    .sort(sort)
     .limit(objectPagination.limitItems)
     .skip(objectPagination.skip);
 
