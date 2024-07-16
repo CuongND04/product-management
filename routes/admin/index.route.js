402b2d10c68261f8bca24acfa5d8b72c9e03d7e6
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-16 22:58:48 +0700

    3. Tạo mới và hiển thị quản lý danh mục

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,9 +1,11 @@
 const systemConfig = require("../../config/system.js");
 const dashboardRouter = require("./dashboard.route.js");
 const productRouter = require("./product.route.js");
+const productCategoryRouter = require("./product-category.route.js");
 
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin;
   app.use(PATH_ADMIN + "/dashboard", dashboardRouter);
   app.use(PATH_ADMIN + "/products", productRouter);
+  app.use(PATH_ADMIN + "/products-category", productCategoryRouter);
 };
