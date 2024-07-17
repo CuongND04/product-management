aed2727c4be5e821fb68f92a80de1552bb1d457d
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-17 15:26:35 +0700

    2.1 Phần thêm danh mục trong tạo mới sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -3,6 +3,9 @@ const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
 const paginationHelper = require("../../helpers/pagination");
 const systemConfig = require("../../config/system");
+const createTreeHelper = require("../../helpers/createTree");
+const ProductCategory = require("../../models/product-category.model");
+
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
   // tạo cái mảng để duyệt mà tạo ra các nút bộ lọc
@@ -137,8 +140,15 @@ module.exports.deleteItem = async (req, res) => {
 
 // [GET] /admin/products/create
 module.exports.create = async (req, res) => {
+  let find = {
+    deleted: false,
+  };
+  const category = await ProductCategory.find(find);
+  const newCategory = createTreeHelper.tree(category);
+
   res.render("admin/pages/products/create.pug", {
     pageTitle: "Thêm mới sản phẩm",
+    category: newCategory,
   });
 };
 // [POST] /admin/products/create
