bab7582d74a747fd62521e91a7fb80dba91feb47
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-17 15:58:39 +0700

    3.2. Xây dựng phần Danh sách nhóm quyền

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -2,10 +2,12 @@ const systemConfig = require("../../config/system.js");
 const dashboardRouter = require("./dashboard.route.js");
 const productRouter = require("./product.route.js");
 const productCategoryRouter = require("./product-category.route.js");
+const roleRoutes = require("./role.route");
 
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin;
   app.use(PATH_ADMIN + "/dashboard", dashboardRouter);
   app.use(PATH_ADMIN + "/products", productRouter);
   app.use(PATH_ADMIN + "/products-category", productCategoryRouter);
+  app.use(PATH_ADMIN + "/roles", roleRoutes);
 };
