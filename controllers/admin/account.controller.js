403c68979e29112ae4916653b2aa255bbe39a4a3
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-18 10:15:53 +0700

    1.3. Làm trang tạo tài khoản, hiển thị danh sách tài khoản

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -1,5 +1,8 @@
 const Account = require("../../models/account.model");
-
+const Role = require("../../models/role.model");
+const systemConfig = require("../../config/system");
+const generateHelper = require("../../helpers/generate.js");
+var md5 = require("md5");
 // [GET] /admin/accounts/
 module.exports.index = async (req, res) => {
   // Find
@@ -8,10 +11,42 @@ module.exports.index = async (req, res) => {
   };
   // End Find
 
-  const records = await Account.find(find);
+  // mảng tài khoản trả về trừ password token
+  const records = await Account.find(find).select("-password -token");
+
+  for (const record of records) {
+    const role = await Role.findOne({
+      _id: record.role_id,
+      deleted: false,
+    });
+
+    record.roleTitle = role.title;
+  }
 
   res.render("admin/pages/accounts/index", {
     pageTitle: "Danh sách tài khoản",
     records: records,
   });
+};
+
+// [GET] /admin/accounts/create
+module.exports.create = async (req, res) => {
+  const roles = await Role.find({
+    deleted: false,
+  });
+  console.log("roles", roles);
+  res.render("admin/pages/accounts/create", {
+    pageTitle: "Thêm mới tài khoản",
+    roles: roles,
+  });
+};
+// [POST] /admin/accounts/create
+module.exports.createPost = async (req, res) => {
+  req.body.password = md5(req.body.password); //mã hóa
+  req.body.token = generateHelper.generateRandomString(30);
+
+  const account = new Account(req.body);
+  await account.save();
+
+  res.redirect(`${systemConfig.prefixAdmin}/accounts`);
 };
--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
 
-const controller = require("../../controllers/admin/account.controller");
+const upload = multer();
+const uploadCloud = require("../../middlewares/admin/uploadsCloud.middleware.js");
+
+const validate = require("../../validates/admin/account.validate.js");
+const controller = require("../../controllers/admin/account.controller.js");
 
 router.get("/", controller.index);
+
+router.get("/create", controller.create);
+
+router.post(
+  "/create",
+  upload.single("avatar"),
+  uploadCloud.upload,
+  validate.createPost,
+  controller.createPost
+);
 
 module.exports = router;
