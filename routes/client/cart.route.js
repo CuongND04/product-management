0ca0e608c33f865c3ec8d85f9f97bfcc12369d35
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-20 20:41:02 +0700

    2.4. Xóa sản phẩm trong giỏ hàng

--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -4,5 +4,5 @@ const router = express.Router();
 const controller = require("../../controllers/client/cart.controller");
 router.get("/", controller.index);
 router.post("/add/:productId", controller.addPost);
-
+router.get("/delete/:productId", controller.deleteItem);
 module.exports = router;
