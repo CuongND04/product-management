1ca025e09a1c906b8bedad6db486d77dee4fa479
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-20 13:04:29 +0700

    1. Trang kết quả tìm kiếm

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,9 +1,11 @@
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 const homeRouter = require("./home.route");
 const productRouter = require("./product.route");
+const searchRoutes = require("./search.route");
 
 module.exports = (app) => {
   app.use(categoryMiddleware.category); // cái nào ở dưới cũng chạy qua cái này
   app.use("/", homeRouter);
   app.use("/products", productRouter);
+  app.use("/search", searchRoutes);
 };
