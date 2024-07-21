23062d8029cef931caba2dd3efd20deb8ad982b0
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-21 15:46:00 +0700

    5. Làm trang 404

--- index.js
+++ index.js
@@ -47,6 +47,11 @@ app.locals.moment = moment;
 
 routeAdmin(app);
 route(app);
+app.get("*", (req, res) => {
+  res.render("client/pages/errors/404", {
+    pageTitle: "404 Not Found",
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
