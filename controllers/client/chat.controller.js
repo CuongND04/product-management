49a2abd88ecd6d3ef4124ed76f30da5433dc1b97
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-22 01:26:25 +0700

    5.2. Làm tính năng chat cơ bản

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,11 +1,38 @@
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
+
 // [GET] /chat/
 module.exports.index = async (req, res) => {
+  const userId = res.locals.user.id;
   // SocketIO
-  _io.on("connection", (socket) => {
-    console.log("Có 1 user kết nối", socket.id);
+  _io.once("connection", (socket) => {
+    socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+      const chat = new Chat({
+        user_id: userId,
+        // room_chat_id: String,
+        content: content,
+        // images: Array,
+      });
+      await chat.save();
+    });
   });
   // END SocketIO
+
+  // Get data from database
+  const chats = await Chat.find({
+    deleted: false,
+  });
+  for (chat of chats) {
+    const infoUser = await User.findOne({
+      _id: chat.user_id,
+    }).select("fullName");
+
+    chat.infoUser = infoUser;
+  }
+  // END Get data from database
+
   res.render("client/pages/chat/index", {
     pageTitle: "Chat",
+    chats: chats,
   });
 };
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,15 @@
-var socket = io();
+// CLIENT_SEND_MESSAGE
+const formSendData = document.querySelector(".chat .inner-form");
+
+if (formSendData) {
+  formSendData.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const content = formSendData.content.value; // name ô input là content
+
+    if (content) {
+      socket.emit("CLIENT_SEND_MESSAGE", content);
+      formSendData.content.value = "";
+    }
+  });
+}
+// End CLIENT_SEND_MESSAGE
