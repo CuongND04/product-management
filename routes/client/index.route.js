f7d0646723994b095afce610dcf45ccb622353f7
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-21 23:56:38 +0700

    3. Làm giao diện chat cho Project

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -4,6 +4,7 @@ const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
 const checkoutRoutes = require("./checkout.route");
 const userRoutes = require("./user.route");
+const chatRoutes = require("./chat.route");
 
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 const cartMiddleware = require("../../middlewares/client/cart.middleware");
@@ -22,4 +23,5 @@ module.exports = (app) => {
   app.use("/cart", cartRoutes);
   app.use("/checkout", checkoutRoutes);
   app.use("/user", userRoutes);
+  app.use("/chat", chatRoutes);
 };
